6eef95532fa4e902a03f9b31c213316531836e99
noahbachmann <noah.bachmann@kauz.ch>
2025-10-23 10:39:25 +0200

    feat: linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,36 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import stylistic from '@stylistic/eslint-plugin'
 
 const eslintConfig = defineConfig([
 	...nextVitals,
-	// Override default ignores of eslint-config-next.
-	globalIgnores([
-		// Default ignores of eslint-config-next:
-		".next/**",
-		"out/**",
-		"build/**",
-	]),
+	{
+		files: [
+			"**/*.{js,ts,jsx,tsx}"
+		],
+		plugins: {
+			'@style': stylistic,
+		},
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
+		rules: {
+			'prefer-const': 'warn',
+			'no-unused-vars': 'warn',
+			'import/no-anonymous-default-export': 'off',
+			'react/no-unescaped-entities': 'off',
+			'react-hooks/exhaustive-deps': 'off',
+
+			'@style/no-trailing-spaces': 'warn',
+			'@style/quotes': ['warn', 'single'],
+			'@style/semi': ['warn', 'never'],
+			'@style/object-curly-spacing': ['warn', 'always'],
+		}
+	}
 ]);
 
 export default eslintConfig;
