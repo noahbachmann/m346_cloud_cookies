d793626181d6f910974a6aeccb25c7e61d9fdfef
noahbachmann <noah.bachmann@kauz.ch>
2025-10-23 10:58:30 +0200

    feat: stylesheet setup

---
+++ app/components/navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar({ }) {
+	return (
+		<div className="w-full h-30 bg-green-500">
+			<p>Hi i'm a navbar</p>
+		</div>
+	)
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
+import './globals.scss'
 
 export const metadata = {
 	title: 'Create Next App',
@@ -21,9 +10,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body>
 				{children}
 			</body>
 		</html>
