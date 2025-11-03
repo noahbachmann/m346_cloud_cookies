5ec5d26726167ae4370e6abbe6d0cbdeb357b475
noahbachmann <noah.bachmann@kauz.ch>
2025-11-03 13:25:40 +0100

    feat: add upgrade logic

--- app/components/gameClient.jsx
+++ app/components/gameClient.jsx
@@ -10,19 +10,6 @@ export default function GameClient({ initialData }) {
 		dataRef.current = data
 	}, [data])
 
-	const idleInterval = setInterval(() => {
-		const additionalClicks = [data.upgrades.upgrade3, data.upgrades.upgrade4*2, data.upgrades.upgrade5*4, data.upgrades.upgrade6*8, data.upgrades.upgrade7*16].reduce(((a,b)=>a+b))
-		const additionalScore = additionalClicks*(data.upgrades.upgrade8+1)
-
-		setData(prev => ({
-			...prev,
-			score: prev.score + additionalScore,
-			highscore: Math.max(prev.score + additionalScore, prev.highscore),
-			total_score: prev.total_score + additionalScore,
-			clicks: prev.clicks + additionalClicks
-		}))
-	}, 1000)
-
 	useEffect(() => {
 		const saveData = async () => {
 			console.log('saving data', dataRef.current)
@@ -32,6 +19,19 @@ export default function GameClient({ initialData }) {
 		const saveInterval = setInterval(() => {
 			saveData()
 		}, 60000)
+
+		const idleInterval = setInterval(() => {
+			const additionalClicks = [dataRef.current.upgrades.upgrade3, dataRef.current.upgrades.upgrade4*2, dataRef.current.upgrades.upgrade5*4, dataRef.current.upgrades.upgrade6*8, dataRef.current.upgrades.upgrade7*16].reduce(((a,b)=>a+b))
+			const additionalScore = additionalClicks*(dataRef.current.upgrades.upgrade8+1)
+
+			setData(prev => ({
+				...prev,
+				score: prev.score + additionalScore,
+				highscore: Math.max(prev.score + additionalScore, prev.highscore),
+				total_score: prev.total_score + additionalScore,
+				clicks: prev.clicks + additionalClicks
+			}))
+		}, 1000)
 
 		return () => {
 			saveData()
@@ -53,25 +53,49 @@ export default function GameClient({ initialData }) {
 		}))
 	}
 
+	function buyUpgrade(upgrade, cost){
+		if(data.score < cost) return
+		setData(prev => ({
+			...prev,
+			score: prev.score - cost,
+			upgrades: {
+				...prev.upgrades,
+				[upgrade]: prev.upgrades[upgrade] + 1
+			}
+		}))
+	}
+
 	return (
 		<>
 			<div>
 				<p>left</p>
 				<p>Points: {data.score}</p>
-				<p>Points/s: {data.score}</p>
-				<p>Click value: {data.upgrades.upgrade1}</p>
+				<p>Points/s: {[data.upgrades.upgrade3, data.upgrades.upgrade4*2, data.upgrades.upgrade5*4, data.upgrades.upgrade6*8, data.upgrades.upgrade7*16].reduce(((a,b)=>a+b))}</p>
+				<p>Click value: {data.upgrades.upgrade1+1}</p>
 				<p>Total Clicks: {data.clicks}</p>
 				<p>Manual Clicks: {data.self_clicks}</p>
 				<p>Automated Clicks: {data.clicks - data.self_clicks}</p>
 				<p>Total Points earned: {data.total_score}</p>
 				<p>Most Points: {data.highscore}</p>
+				<p>Prestige Stage: 0</p>
 				<p>Upgrades purchased: {Object.values(data.upgrades).reduce((a,b) => a+b)}</p>
 			</div>
 			<div>
 				<button onClick={ click }>Click Me</button>
 			</div>
 			<div>
-				<p>right</p>
+				{
+					Object.entries(data.upgrades).map(([upgrade, level], index) => {
+						const cost = Math.floor(20 * (level+1) * Math.pow((index + 1),2))
+
+						return (
+							<div className="flex" key={ index }>
+								<p>{ upgrade } - Level: { level }</p>
+								<button onClick={ () => buyUpgrade(upgrade, cost) }>{ cost }</button>
+							</div>
+						)
+					})
+				}
 			</div>
 		</>
 	)
