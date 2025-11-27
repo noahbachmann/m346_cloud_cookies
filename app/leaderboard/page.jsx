14a1ccb344ba5973117f779db6e73963b53993a0
D3dZone <paul.willimann@gmx.ch>
2025-11-27 08:18:36 +0100

    feat: create leaderboard design

--- app/leaderboard/page.jsx
+++ app/leaderboard/page.jsx
@@ -10,15 +10,27 @@ export default async function Leaderboard({}){
 		.limit(10)
 
 	return(
-		<div>
-			<p>Leaderboard page</p>
-			{
-				Object.values(users).map((user, index) => (
-					<div key={ index }>
-						<p>{ index + 1 }. { user.name }: { user.gameState.total_score }</p>
-					</div>
-				))
-			}
+		<div className="container container-sm flex flex-col gap-32">
+			<h3 className="self-center">Top 10 Players</h3>
+			<div className="relative px-36 py-40 bg-primary border-dark border-2 rounded">
+				<div className="absolute top-[10%] justify-self-center h-[80%] w-6 bg-dark rounded"></div>
+				
+				<div className="flex justify-between pb-38">
+					<div>User</div>
+					<div>Score</div>
+				</div>
+
+				<div className="flex flex-col gap-12">
+					{
+						Object.values(users).map((user, index) => (
+							<div className="flex justify-between" key={ index }>
+								<p>{ index + 1 }. { user.name }</p>
+								<p>{ user.gameState.total_score }</p>
+							</div>
+						))
+					}
+				</div>
+			</div>
 		</div>
 	)
 }
